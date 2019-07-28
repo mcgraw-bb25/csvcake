72c9deb52ffbc8d6e09870112f9550ec768ce74b
mcgraw-bb25 <my.git.user@nym.hush.com>
2019-07-28 15:31:24 -0400

    drop the second list comp

--- python/main.py
+++ python/main.py
@@ -1,12 +1,12 @@
 import csv
 
-filename = "../test.csv"
+filename = "./test.csv"
 
 if __name__ == "__main__":
     
     with open(filename, "r") as csvin:
         reader = csv.DictReader(csvin)
-        rows = [row for row in reader]
+        # rows = [row for row in reader]
+        age = sum([int(row["age"]) for row in reader])
 
-    age = sum([int(row["age"]) for row in rows])
     print("Total age: {}".format(age))
