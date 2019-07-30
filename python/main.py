9473473ede3d732968f3d36c27b5b6f6065a7480
mcgraw-bb25 <my.git.user@nym.hush.com>
2019-07-29 21:17:21 -0400

    remove unused statement

--- python/main.py
+++ python/main.py
@@ -1,12 +1,11 @@
 import csv
 
-filename = "./test.csv"
+filename = "./test2.csv"
 
 if __name__ == "__main__":
     
     with open(filename, "r") as csvin:
         reader = csv.DictReader(csvin)
-        # rows = [row for row in reader]
         age = sum([int(row["age"]) for row in reader])
 
     print("Total age: {}".format(age))
